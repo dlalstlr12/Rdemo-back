f2bcd75d097302ed9898ee9d96eaf4226cf17b58
dlalstlr12 <as2984348@naver.com>
2025-09-17 16:20:13 +0900

    build : 빌드 파일 및 초기 세팅

---
+++ src/main/java/com/example/Pdemo_backend/PdemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pdemo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdemoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pdemo_backend/PdemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pdemo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
